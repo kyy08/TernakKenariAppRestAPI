34d608c7ee6b6ddcbc499ece2e33587a76933540
kyy08 <kyyelzz1@gmail.com>
2025-06-06 23:59:24 +0700

    membuat buyer_profil_screen

---
+++ lib/presentation/buyer/profile/buyer_profile_screen.dart
@@ -0,0 +1,56 @@
+import 'package:canary_101/presentation/buyer/profile/bloc/profile_buyer_bloc.dart';
+import 'package:canary_101/presentation/buyer/profile/widget/profile_view_buyer.dart';
+import 'package:canary_101/presentation/buyer/profile/widget/profile_widget_form.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+class BuyerProfileScreen extends StatefulWidget {
+  const BuyerProfileScreen({super.key});
+
+  @override
+  State<BuyerProfileScreen> createState() => _BuyerProfileScreenState();
+}
+
+class _BuyerProfileScreenState extends State<BuyerProfileScreen> {
+  @override
+  initState() {
+    super.initState();
+    // Ambil profil pembeli saat halaman dimuat
+    context.read<ProfileBuyerBloc>().add(GetProfileBuyerEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Profil Pembeli")),
+      body: BlocListener<ProfileBuyerBloc, ProfileBuyerState>(
+        listener: (context, state) {
+          if (state is ProfileBuyerAdded) {
+            // Refresh profil setelah tambah
+            context.read<ProfileBuyerBloc>().add(GetProfileBuyerEvent());
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text("Profil berhasil ditambahkan")),
+            );
+          }
+        },
+        child: BlocBuilder<ProfileBuyerBloc, ProfileBuyerState>(
+          builder: (context, state) {
+            if (state is ProfileBuyerLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (state is ProfileBuyerLoaded &&
+                state.profile.data.name.isNotEmpty) {
+              final profile = state.profile.data;
+              return ProfileViewBuyer(profile: profile);
+            }
+
+            // Default ke form jika tidak ada data atau error
+            return ProfileBuyerInputForm();
+          },
+        ),
+      ),
+    );
+  }
+}
